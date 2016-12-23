6293c222e53b5ace2be723b0de5a4bdeeedd9725
zhh <617146873@qq.com>
2016-12-23 21:37:57 +0800

    改为商品管理系统

---
+++ public/js/menu.js
@@ -0,0 +1,13 @@
+// 这里放菜单的点击相应函数
+
+function employeeInfo(){
+    alert('employeeInfo');
+}
+
+function customerInfo(){
+    alert('customerInfo');
+}
+
+function productInfo(){
+    alert('productInfo');
+}
